94d2bd9a7d822bec3f4ffb00c855a05572194f28
Jônatas A. <hello@jnaraujo.com>
2025-05-21 23:59:19 -0300

    feat: add config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,8 +1,18 @@
 package config
 
 const (
+	// Application configuration
 	AppID      = "seekr"
 	AppName    = "SeekR"
 	AppVersion = 0.1
 	DBFileExt  = "skdb"
+)
+
+const (
+	// Embedding configuration settings
+	EmbeddingDimension    = 768
+	MaxChunkChars         = 500
+	ChunkOverlapping      = 100
+	DefaultEmbeddingModel = "nomic-embed-text"
+	MaxContent            = 10_000
 )
