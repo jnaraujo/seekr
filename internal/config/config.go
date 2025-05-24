4ca0e94119ae58625e6913ee094fdc667f50f713
Jônatas A. <hello@jnaraujo.com>
2025-05-24 11:32:02 -0300

    feat: update default embedding model to the latest version

--- internal/config/config.go
+++ internal/config/config.go
@@ -10,9 +10,10 @@ const (
 
 const (
 	// Embedding configuration settings
-	EmbeddingDimension    = 768
-	MaxChunkChars         = 500
-	ChunkOverlapping      = 100
-	DefaultEmbeddingModel = "nomic-embed-text"
+	EmbeddingDimension = 768
+	MaxChunkChars      = 500
+	ChunkOverlapping   = 100
+	// ollama pull hf.co/nomic-ai/nomic-embed-text-v2-moe-gguf
+	DefaultEmbeddingModel = "hf.co/nomic-ai/nomic-embed-text-v2-moe-gguf"
 	MaxContent            = 10_000
 )
