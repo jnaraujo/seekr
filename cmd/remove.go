c0ed4b21eed14acd126055e3483326aefaec6ff1
Jônatas A. <hello@jnaraujo.com>
2025-05-24 12:04:18 -0300

    feat: enhance remove command to support directory removal with file iteration

--- cmd/remove.go
+++ cmd/remove.go
@@ -5,6 +5,7 @@ import (
 	"path/filepath"
 
 	"github.com/jnaraujo/seekr/internal/id"
+	"github.com/jnaraujo/seekr/internal/storage"
 	"github.com/spf13/cobra"
 )
 
@@ -21,12 +22,39 @@ var removeCmd = &cobra.Command{
 			return
 		}
 
-		if err := store.Remove(cmd.Context(), id.HashPath(inputPath)); err != nil {
-			fmt.Printf("failed to remove document %q: %v\n", inputPath, err)
-			return
+		pathKind, _ := storage.CheckPath(inputPath)
+		// we ignore the error because the document may not necessarily exist anymore, but we might still want to remove it from the index.
+		switch pathKind {
+		case storage.DirectoryPathKind:
+			fmt.Printf("Removing directory %q...\n", inputPath)
+
+			files, err := storage.FilePathWalkDir(inputPath)
+			if err != nil {
+				fmt.Printf("Failed to remove directory %q: %v\n", inputPath, err)
+				return
+			}
+
+			for _, file := range files {
+				fmt.Printf("Removing document %q...\n", file)
+				err = store.Remove(cmd.Context(), id.HashPath(file))
+				if err != nil {
+					fmt.Printf("Failed to remove %q: %v\n", file, err)
+					continue
+				}
+				fmt.Printf("Document %q removed successfully!\n", file)
+			}
+
+			fmt.Printf("Directory %q removed\n", inputPath)
+		default:
+			fmt.Printf("Removing file %q...\n", inputPath)
+			err := store.Remove(cmd.Context(), id.HashPath(inputPath))
+			if err != nil {
+				fmt.Printf("failed to remove document %q: %v\n", inputPath, err)
+				return
+			}
+			fmt.Println("Document removed successfully")
 		}
 
-		fmt.Println("Document removed successfully")
 	},
 }
 
