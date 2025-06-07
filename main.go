0fd2db101411ec4d3f199e869b201b34885d7537
Jônatas A. <hello@jnaraujo.com>
2025-06-07 18:48:54 -0300

    feat: add CPU profiling support to main application

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "github.com/jnaraujo/seekr/cmd"
+import (
+	"log"
+	"os"
+	"runtime/pprof"
+
+	"github.com/jnaraujo/seekr/cmd"
+)
 
 func main() {
+	// cleanup := profileApp()
+	// defer cleanup()
 	cmd.Execute()
+}
+
+func profileApp() func() {
+	cpuFile, err := os.Create("cpu.prof")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := pprof.StartCPUProfile(cpuFile); err != nil {
+		log.Fatal(err)
+	}
+
+	return func() {
+		pprof.StopCPUProfile()
+		cpuFile.Close()
+	}
 }
